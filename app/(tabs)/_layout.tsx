a6b5a6da07bc4e8eb8d4a8a3dfcff0094bc5e7b7
louis <louis.hocquet@sfr.fr>
2026-01-23 12:38:58 +0100

    poc - Add expo navigation + Dark Mode + Home and Profile Styles

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabsLayout = () => {
+  const { colors } = useTheme();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
+        tabBarStyle: {
+          backgroundColor: colors.surface,
+          borderTopWidth: 1,
+          borderTopColor: colors.border,
+          height: 90,
+          paddingBottom: 24,
+          paddingTop: 12,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: 600,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ components/Preferences.tsx
@@ -0,0 +1,41 @@
+import { createProfileStyles } from "@/assets/styles/profile.styles";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { Switch, Text, View } from "react-native";
+
+const Preferences = () => {
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+  const profileStyles = createProfileStyles(colors);
+  return (
+    <LinearGradient
+      colors={colors.gradients.surface}
+      style={profileStyles.section}
+    >
+      <Text style={profileStyles.sectionTitle}>Preferences</Text>
+
+      {/* DARK MODE */}
+      <View style={profileStyles.settingItem}>
+        <View style={profileStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.primary}
+            style={profileStyles.settingIcon}
+          >
+            <Ionicons name="moon" color={"#fff"} size={18} />
+          </LinearGradient>
+          <Text style={profileStyles.settingText}>Mode sombre</Text>
+        </View>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleDarkMode}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.primary }}
+          ios_backgroundColor={colors.border}
+        />
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default Preferences;
